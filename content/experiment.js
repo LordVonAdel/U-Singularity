490e8af87f5ca1763e91cd12f9fac7a3b89a9fd4
LordVonAdel <wuppinana@web.de>
2018-01-20 12:43:22 +0100

    singularity is now destroying tiles

--- content/experiment.js
+++ content/experiment.js
@@ -1,20 +1,33 @@
 module.exports = {
   objects: {
     singularity: {
-      image: [{number: 1, source: "objects/singularity.png", width: 96, height: 96, visible: false}],
+      image: [{number: 1, source: "objects/singularity.png", width: 96, x: -32, y: -32, height: 96, visible: false}],
       sync: {
-        nextMoveIn: 1
+        active: false
       },
       start(){
         this.changeSprite(0, {visible: true});
         this.update();
+        this.sync.active = true;
       },
-      step(delta){
-        this.sync.nextMoveIn -= delta;
-        if (this.sync.nextMoveIn <= 0){
-          this.sync.nextMoveIn = 5;
-          var dir = Math.floor(Math.random()*4);
-          this.moveDir(dir, 10);
+      onStep(delta){
+        if (!this.sync.active) return;
+
+        var dir = Math.floor(Math.random()*4);
+        this.moveDir(dir, 1);
+
+        for (var i = 0; i < 3; i++){
+          for (var j = 0; j < 3; j++){
+            var x = this.tx + i - 1;
+            var y = this.ty + j - 1;
+            var tile = this.world.cellGet(x, y);
+            //this.world.cellSet(x, y, 1);
+            if (["1", "2", "3", "7"].includes(tile)){
+              this.world.cellSet(x, y, 5);
+            }else if (["4", "6", "8"].includes(tile)){
+              this.world.cellSet(x, y, 10);
+            }
+          }
         }
       }
     }
