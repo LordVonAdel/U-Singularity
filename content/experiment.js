d2c34ce21da720a464969ff8888414d73ab678fa
LordVonAdel <wuppinana@web.de>
2018-01-21 10:10:27 +0100

    made the singularity slower

--- content/experiment.js
+++ content/experiment.js
@@ -3,7 +3,8 @@ module.exports = {
     singularity: {
       image: [{number: 1, source: "objects/singularity.png", width: 96, x: -32, y: -32, height: 96, visible: false}],
       sync: {
-        active: false
+        active: false,
+        nextMoveIn: 1000
       },
       start(){
         this.changeSprite(0, {visible: true});
@@ -13,19 +14,23 @@ module.exports = {
       onStep(delta){
         if (!this.sync.active) return;
 
-        var dir = Math.floor(Math.random()*4);
-        this.moveDir(dir, 1);
-
-        for (var i = 0; i < 3; i++){
-          for (var j = 0; j < 3; j++){
-            var x = this.tx + i - 1;
-            var y = this.ty + j - 1;
-            var tile = this.world.cellGet(x, y);
-            //this.world.cellSet(x, y, 1);
-            if (["1", "2", "3", "7"].includes(tile)){
-              this.world.cellSet(x, y, 5);
-            }else if (["4", "6", "8"].includes(tile)){
-              this.world.cellSet(x, y, 10);
+        this.sync.nextMoveIn -= delta;
+        if (this.sync.nextMoveIn <= 0){
+          this.sync.nextMoveIn = 200;
+          var dir = Math.floor(Math.random()*4);
+          this.moveDir(dir, 1);
+  
+          for (var i = 0; i < 3; i++){
+            for (var j = 0; j < 3; j++){
+              var x = this.tx + i - 1;
+              var y = this.ty + j - 1;
+              var tile = this.world.cellGet(x, y);
+              //this.world.cellSet(x, y, 1);
+              if (["1", "2", "3", "7"].includes(tile)){
+                this.world.cellSet(x, y, 5);
+              }else if (["4", "6", "8"].includes(tile)){
+                this.world.cellSet(x, y, 10);
+              }
             }
           }
         }
